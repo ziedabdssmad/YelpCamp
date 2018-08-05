d489984e4cb5df8e2b141525d9a438eb25dae810
unknown <ziedabdssmad123@gmail.com>
2018-08-05 14:47:50 +0200

    adding a render route and create first ejs file

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require("express");
 var app = express();
 
+app.set("view engine","ejs");
+
 app.get("/", function (req,res) {
-    res.send("this will be the main page soon !!");
+    res.render("landing");
 })
 
 app.listen(3000,function () {
